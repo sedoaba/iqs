5ab9123d57af83148b6f6f65b2ace7ec72774a84
Lebohang Sedoaba <lebohang.sedoaba@britehouse.co.za>
2022-06-09 21:15:57 +0200

    add forgot password

--- iqs-app/src/app/pages/forgotpassword/forgotpassword.component.ts
+++ iqs-app/src/app/pages/forgotpassword/forgotpassword.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotpasswordComponent implements OnInit {
 
+  username = '';
+
+
   constructor() { }
+  
 
   ngOnInit(): void {
+  }
+
+  btnForgotPassword(){
+
   }
 
 }
